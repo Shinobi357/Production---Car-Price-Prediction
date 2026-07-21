2028c6ca020d4d8f8e30e5da8db19438a74eebaf
Shinobi357 <Calfpark85@gmail.com>
2026-07-21 11:32:25 -0400

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -6,13 +6,13 @@ import pandas as pd
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 MODEL_PATH = BASE_DIR / "models" / "car_price_model.pkl"
-PREPROCESSOR_PATH = BASE_DIR / "models" / "preprocessor.pkl"
 
-model = joblib.load(MODEL_PATH)
-preprocessor = joblib.load(PREPROCESSOR_PATH)
+model_pipeline = joblib.load(MODEL_PATH)
 
 
-def predict_price(data):
+def predict_price(data: dict) -> float:
     input_df = pd.DataFrame([data])
-    transformed = preprocessor.transform(input_df)
-    return model.predict(transformed)[0]
+
+    prediction = model_pipeline.predict(input_df)[0]
+
+    return float(prediction)
